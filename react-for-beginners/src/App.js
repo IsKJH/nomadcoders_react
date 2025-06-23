a790ed0a2127e1617e3a59243cd31b27003edb77
IsKJH <dkdlrh147@gmail.com>
2025-06-23 14:55:44 +0900

    학습

---
+++ react-for-beginners/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import {useEffect, useState} from "react";
+
+function Hello() {
+    useEffect(() => {
+        console.log("created :)");
+        return () => console.log("destoryed :(");
+    }, []);
+    return <h1>안녕하세요!</h1>;
+}
+
+function App() {
+    const [showing, setShowing] = useState(false);
+    const onClick = () => setShowing((prev) => !prev);
+    return (
+        <div className="App">
+            {showing ? <Hello/> : null}
+            <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+        </div>);
+}
+
+export default App;
