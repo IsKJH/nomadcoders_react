a5e0a51b90107ed1f116870411cdcb6d798003ef
IsKJH <dkdlrh147@gmail.com>
2025-06-24 11:50:14 +0900

    학습

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,38 +1,91 @@
 import './App.css';
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
-    const [toDo, setTodo] = useState("");
-    const [toDos, setTodos] = useState([]);
-    const onChange = (event) => {
-        setTodo(event.target.value);
-    }
-    const onSubmit = (event) => {
-        event.preventDefault();
-        if (toDo === "") {
-            return;
-        }
-        setTodo("");
-        setTodos(currentArray => [toDo, ...currentArray]);
-    }
-    console.log(toDos);
-    return (
-        <div className="App">
-            <h1>My To Dos ({toDos.length})</h1>
-            <form onSubmit={onSubmit}>
-                <input
-                    onChange={onChange}
-                    value={toDo} type="text"
-                    placeholder="Write your to do..."/>
-                <button>Add To Do</button>
-                <ul>
-                    {toDos.map((item, index) => (
-                            <li key={index}>{item}</li>
-                        )
-                    )}
-                </ul>
-            </form>
+    const [loading, setLoading] = useState(true);
+    const [coins, setCoins] = useState([]);
+    const [myCoins, setMyCoins] = useState([]);
+    const [budget, setBudget] = useState(0);
+    const [selectCoin, setSelectCoin] = useState([]);
 
+    useEffect(() => {
+        fetch("https://api.coinpaprika.com/v1/tickers").then(
+            (response) => response.json()).then((json) => {
+            setCoins(json);
+            setLoading(false);
+            setSelectCoin(json[0]);
+        });
+    }, []);
+
+    const onChangeSelectCoin = (e) => {
+        const selectedCoin = coins.find((coin) => coin.id === e.target.value);
+        setSelectCoin(selectedCoin);
+    }
+
+    const onChangeBudget = (e) => {
+        setBudget(e.target.value);
+    };
+
+    const onClickBuyCoin = () => {
+        const coinPrice = selectCoin.quotes.USD.price;
+        const coinAmount = budget / coinPrice;
+        const existingCoinIndex = myCoins.findIndex(coin => coin.id === selectCoin.id);
+        if (existingCoinIndex >= 0) {
+            const updatedCoins = [...myCoins];
+            updatedCoins[existingCoinIndex] = {
+                ...updatedCoins[existingCoinIndex],
+                amount: updatedCoins[existingCoinIndex].amount + coinAmount,
+                investedAmount: updatedCoins[existingCoinIndex].investedAmount + parseFloat(budget)
+            }
+            setMyCoins(updatedCoins);
+        } else {
+            const purchaseInfo = {
+                ...selectCoin,
+                amount: coinAmount,
+                investedAmount: parseFloat(budget)
+            }
+            setMyCoins([purchaseInfo, ...myCoins]);
+        }
+        setBudget(0);
+    }
+
+    return (
+
+        <div className="App">
+            <div>
+                <h1>The Coins! ({coins.length})</h1>
+                {loading ? <strong>Loading...</strong> :
+                    <select onChange={onChangeSelectCoin}>
+                        {coins.map((coin) => (
+                            <option value={coin.id}
+                                    key={coin.id}>{coin.name} ({coin.symbol}): {coin.quotes.USD.price}</option>
+                        ))}
+                    </select>}
+                <div>
+                    <input value={budget} onChange={onChangeBudget} placeholder="$ 예산을 입력하세요."/>
+                    <button onClick={onClickBuyCoin} disabled={budget === 0}>구매</button>
+                </div>
+                <div>
+                    <table style={{margin: "0 auto"}}>
+                    <thead>
+                        <tr>
+                            <th>이름</th>
+                            <th>개수</th>
+                            <th>구매 총 가격</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {myCoins.map((coin, index) => (
+                            <tr key={index}>
+                                <td>{coin.name}</td>
+                                <td>{coin.amount}</td>
+                                <td>{coin.investedAmount}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 }
